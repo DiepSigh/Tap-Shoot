bb15bd3513c90634096a2d552a24913068cae11d
Diadxm <r.berenguer19@hotmail.com>
2019-04-09 12:51:59 -0400

    pushing to my pc
    switched computers to do work at home

--- scripts/EnemySpawn.js
+++ scripts/EnemySpawn.js
@@ -2,27 +2,54 @@ var Spawner = (function(){
 
     var x, y, topLeft, topRight, botLeft, botRight, image;
 
-    
+    function FillTopLeft(sprite){
+        topLeft = [];
 
+        for (i=0;i<4;i++){
+            topLeft.push(sprite);
+        }
+    }
 
-function preload(){
-    game.load.image('sprite', 'images/bidoff.jpg');
-}
+    function fillTopRight(sprite){
+        topRight = [];
 
-function create(){
-    var test = game.add.sprite(0,0,'sprite');
-}
+        for (i=0;i<4;i++){
+            topRight.push(sprite);
+        }
+    }
 
-function LoadSprite(){
+    function fillbotLeft(sprite){
+        botLeft = [];
 
+        for (i=0;i<4;i++){
+            botLeft.push(sprite);
+        }
+    }
+
+    function fillbotRight(sprite){
+        botRight = [];
+
+        for (i=0;i<4;i++){
+            botRight.push(sprite);
+        }
+    }
+
+    function RemoveEnemyTL(){
+        delete topLeft[0];
+    }
+
+    function RemoveEnemyTR(){
+        delete topRight[0];
+    }
+
+    function RemoveEnemyBL(){
+        delete botLeft[0];
+    }
+
+    function RemoveEnemyBR(){
+        delete botRight[0];
     }
 
 
-    var ctx = document.getElementById("MyCanvas");
 
-
-
-    return {
-
-    };
 })();
